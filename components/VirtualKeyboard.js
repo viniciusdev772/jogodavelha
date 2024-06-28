dc0030c8333414456a1661a1cd32668268843b73
ViniciusDev <viniciusdev37@gmail.com>
2024-06-28 00:02:22 -0300

    d

--- components/VirtualKeyboard.js
+++ components/VirtualKeyboard.js
@@ -2,17 +2,14 @@ import React from "react";
 
 const keys = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split("");
 
-export default function VirtualKeyboard({ onKeyPress, guessedLetters }) {
+export default function VirtualKeyboard({ onKeyPress }) {
   return (
     <div className="grid grid-cols-9 gap-2">
       {keys.map((key) => (
         <button
           key={key}
           onClick={() => onKeyPress(key)}
-          disabled={guessedLetters.includes(key)}
-          className={`${
-            guessedLetters.includes(key) ? "bg-gray-500" : "bg-gray-300"
-          } text-black font-bold py-2 px-4 rounded`}
+          className="bg-gray-300 text-black font-bold py-2 px-4 rounded"
         >
           {key}
         </button>
